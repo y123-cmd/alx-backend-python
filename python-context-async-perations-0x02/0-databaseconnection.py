4e11579d461e03ef1557b6b25b77d6acaba28ae7
Yvonne <81848589+y123-cmd@users.noreply.github.com>
2025-07-11 19:00:45 +0300

    added class-based context manager DatabaseConnection for DB handling

---
+++ python-context-async-perations-0x02/0-databaseconnection.py
@@ -0,0 +1,22 @@
+import sqlite3
+
+class DatabaseConnection:
+    def __init__(self, db_name):
+        self.db_name = db_name
+        self.conn = None
+
+    def __enter__(self):
+        self.conn = sqlite3.connect(self.db_name)
+        return self.conn  
+
+    def __exit__(self, exc_type, exc_value, traceback):
+        if self.conn:
+            self.conn.close()
+
+
+with DatabaseConnection('users.db') as conn:
+    cursor = conn.cursor()
+    cursor.execute("SELECT * FROM users")
+    results = cursor.fetchall()
+    for row in results:
+        print(row)
