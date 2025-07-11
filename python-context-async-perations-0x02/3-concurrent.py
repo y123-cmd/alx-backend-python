ef9ee9e9db40b8a9f85a5d1aefadb7e97d6b1acf
Yvonne <81848589+y123-cmd@users.noreply.github.com>
2025-07-12 00:10:07 +0300

    fix:added return in async_fetch_users and async_fetch_older_users

--- python-context-async-perations-0x02/3-concurrent.py
+++ python-context-async-perations-0x02/3-concurrent.py
@@ -6,26 +6,29 @@ async def async_fetch_users():
     async with aiosqlite.connect("users.db") as db:
         async with db.execute("SELECT * FROM users") as cursor:
             results = await cursor.fetchall()
-            print("All Users:")
-            for row in results:
-                print(row)
+            return results
 
 
 async def async_fetch_older_users():
     async with aiosqlite.connect("users.db") as db:
         async with db.execute("SELECT * FROM users WHERE age > 40") as cursor:
             results = await cursor.fetchall()
-            print("Users older than 40:")
-            for row in results:
-                print(row)
-
+            return results
 
 async def fetch_concurrently():
-    await asyncio.gather(
+    all_users, older_users = await asyncio.gather(
         async_fetch_users(),
         async_fetch_older_users()
     )
 
+    print("All Users:")
+    for row in all_users:
+        print(row)
+
+    print("\nUsers older than 40:")
+    for row in older_users:
+        print(row)
 
 if __name__ == "__main__":
     asyncio.run(fetch_concurrently())
+
