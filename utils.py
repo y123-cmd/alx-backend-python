d39c4c1e5cf23ff4b65c1d05fb6089f36f912bfe
Yvonne <81848589+y123-cmd@users.noreply.github.com>
2025-07-25 18:36:45 +0300

    added unittest project

--- utils.py
+++
@@ -1,9 +0,0 @@
-
-"""Utilities module with access_nested_map function."""
-
-
-def access_nested_map(nested_map, path):
-    """Accesses a value in a nested dictionary using a sequence of keys."""
-    for key in path:
-        nested_map = nested_map[key]
-    return nested_map
