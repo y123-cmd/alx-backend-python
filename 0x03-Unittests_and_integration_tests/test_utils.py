6d80e68e1f3b55c10126c0e05a04d103913db2a0
Yvonne <81848589+y123-cmd@users.noreply.github.com>
2025-07-25 17:21:35 +0300

    added utils.py

--- 0x03-Unittests_and_integration_tests/test_utils.py
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -1,3 +1,6 @@
+import sys
+import os
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), "..")))
 import unittest
 from parameterized import parameterized
 from utils import access_nested_map 
---
+++ utils.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+"""Utilities module with access_nested_map function."""
+
+
+def access_nested_map(nested_map, path):
+    """Accesses a value in a nested dictionary using a sequence of keys."""
+    for key in path:
+        nested_map = nested_map[key]
+    return nested_map
