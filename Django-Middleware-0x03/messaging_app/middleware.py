293743e7aae2cea2047cba169c20b45f1b6c239e
Yvonne <81848589+y123-cmd@users.noreply.github.com>
2025-07-27 15:57:47 +0300

    added the project in correct directory

---
+++ Django-Middleware-0x03/messaging_app/middleware.py
@@ -0,0 +1,15 @@
+from datetime import datetime
+
+class RequestLoggerMiddleware:
+    def __init__(self, get_response):
+        self.get_response = get_response
+
+    def __call__(self, request):
+        user = request.user if request.user.is_authenticated else 'Anonymous'
+        log_line = f"{datetime.now()} - User: {user} - Path: {request.path}\n"
+
+        with open("requests.log", "a") as log_file:
+            log_file.write(log_line)
+
+        response = self.get_response(request)
+        return response
