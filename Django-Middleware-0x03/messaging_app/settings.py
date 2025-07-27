85aed0792f65fd35e62e1cbf2cbd8a8b22a6b4fb
Yvonne <81848589+y123-cmd@users.noreply.github.com>
2025-07-27 15:36:16 +0300

    added requirements.txt with drf with drf-nested-routers

--- Django-Middleware-0x03/messaging_app/settings.py
+++ Django-Middleware-0x03/messaging_app/settings.py
@@ -95,6 +95,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'chats.middleware.RequestLoggingMiddleware',
 ]
 
 ROOT_URLCONF = 'messaging_app.urls'
