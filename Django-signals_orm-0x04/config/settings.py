0c3b7db24c1cc5c467ebfcfe1ec60e2723c31142
Yvonne <81848589+y123-cmd@users.noreply.github.com>
2025-08-01 13:00:59 +0300

    added comma to my models.py

--- Django-signals_orm-0x04/config/settings.py
+++ Django-signals_orm-0x04/config/settings.py
@@ -38,7 +38,7 @@ INSTALLED_APPS = [
     "django.contrib.messages",
     "django.contrib.staticfiles",
     "messaging.apps.MessagingConfig",
-    "Django_Chat",
+   # "Django_Chat",
 ]
 
 MIDDLEWARE = [
