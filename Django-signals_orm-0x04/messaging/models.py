1a795562e0bdde7b593acc354f9c028ea7dbc2b6
Yvonne <81848589+y123-cmd@users.noreply.github.com>
2025-08-01 12:00:58 +0300

    track message edits pre_save and log to messagehistory

--- Django-signals_orm-0x04/messaging/models.py
+++ Django-signals_orm-0x04/messaging/models.py
@@ -6,6 +6,7 @@ class Message(models.Model):
     receiver = models.ForeignKey(User, on_delete=models.CASCADE, related_name='received_messages')
     content = models.TextField()
     timestamp = models.DateTimeField(auto_now_add=True)
+    edited = models.BooleanField(default=False)
 
     def __str__(self):
         return f'Message from {self.sender} to {self.receiver}'
@@ -18,3 +19,11 @@ class Notification(models.Model):
 
     def __str__(self):
         return f'Notification for {self.user} - Message ID: {self.message.id}'
+
+class MessageHistory(models.Model):
+    message = models.ForeignKey(Message, on_delete=models.CASCADE, related_name='history')
+    old_content = models.TextField()
+    edited_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f'History of message ID {self.message.id}'
--- Django-signals_orm-0x04/messaging/signals.py
+++ Django-signals_orm-0x04/messaging/signals.py
@@ -1,8 +1,24 @@
-from django.db.models.signals import post_save
+from django.db.models.signals import post_save, pre_save
 from django.dispatch import receiver
-from .models import Message, Notification
+from .models import Message, Notification, MessageHistory
 
 @receiver(post_save, sender=Message)
 def create_notification(sender, instance, created, **kwargs):
     if created:
         Notification.objects.create(user=instance.receiver, message=instance)
+
+@receiver(pre_save, sender=Message)
+def log_message_edit(sender, instance, **kwargs):
+    if instance.pk:  
+        try:
+            old_message = Message.objects.get(pk=instance.pk)
+            if old_message.content != instance.content:
+                
+                MessageHistory.objects.create(
+                    message=instance,
+                    old_content=old_message.content
+                )
+                
+                instance.edited = True
+        except Message.DoesNotExist:
+            pass 
