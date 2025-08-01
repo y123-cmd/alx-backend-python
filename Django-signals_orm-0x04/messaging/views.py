00db73b730e9f96de7536428e0f3f8ae9c041115
Yvonne <81848589+y123-cmd@users.noreply.github.com>
2025-08-01 13:07:32 +0300

    Add parent_message field and optimize message querying with select_related and prefetch_related

--- Django-signals_orm-0x04/messaging/views.py
+++ Django-signals_orm-0x04/messaging/views.py
@@ -1,10 +1,29 @@
+from django.shortcuts import render, redirect, get_object_or_404
 from django.contrib.auth.models import User
-from django.shortcuts import redirect
 from django.contrib.auth.decorators import login_required
+from .models import Message
 
 @login_required
-def delete_user(request):
-    user = request.user
-    user.delete()
-    return redirect('/')  
+def send_message(request):
+    if request.method == 'POST':
+        receiver_id = request.POST.get('receiver_id')
+        content = request.POST.get('content')
+        parent_id = request.POST.get('parent_id')  
+
+        receiver = get_object_or_404(User, id=receiver_id)
+        parent_message = None
+
+        if parent_id:
+            parent_message = Message.objects.get(id=parent_id)
+
+        Message.objects.create(
+            sender=request.user,          
+            receiver=receiver,            
+            content=content,
+            parent_message=parent_message
+        )
+        return redirect('inbox')  
+
+    users = User.objects.exclude(id=request.user.id)
+    return render(request, 'messaging/send_message.html', {'users': users})
 
