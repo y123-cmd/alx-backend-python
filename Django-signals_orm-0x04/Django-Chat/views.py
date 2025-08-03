3d292bfe56d281be10fa74f16b9db5595e943633
Yvonne <81848589+y123-cmd@users.noreply.github.com>
2025-08-03 13:52:31 +0300

    Added 60-second cache to conversation view and configure LocMemCache

--- Django-signals_orm-0x04/Django-Chat/views.py
+++ Django-signals_orm-0x04/Django-Chat/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
+from .models import Message
+from django.views.decorators.cache import cache_page
 
-# Create your views here.
+@cache_page(60)  # Cache this view for 60 seconds
+def conversation_messages(request, conversation_id):
+    messages = Message.objects.filter(conversation_id=conversation_id)
+    return render(request, 'chats/conversation.html', {'messages': messages})
+
