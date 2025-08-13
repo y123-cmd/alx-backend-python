63317906bd5d5f3a158c84d318dbc2f6cb89e96c
Yvonne <81848589+y123-cmd@users.noreply.github.com>
2025-08-13 14:43:12 +0300

    Add kurbeScript to start and verify Kubernetes cluster

--- messaging_app/messaging_app/settings.py
+++ messaging_app/messaging_app/settings.py
@@ -133,7 +133,7 @@ WSGI_APPLICATION = 'messaging_app.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql',
-        'NAME': os.getenv("MYSQL_DATABASE"),
+        'NAME': os.getenv("MYSQL_DB"),
         "USER": os.getenv("MYSQL_USER"),
         "PASSWORD": os.getenv("MYSQL_PASSWORD"),
         "HOST": os.getenv("DB_HOST", "db"),
